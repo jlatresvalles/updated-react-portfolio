822a0ee898b895f928becf24cf0647f35599f3ce
jlatresvalles <jlatresvalles@gmail.com>
2025-12-16 12:42:21 -0500

    Added some of introduction, name is not centered

---
+++ src/components/sections/Home.jsx
@@ -0,0 +1,37 @@
+export const Home = () => {
+    return (
+        <section 
+            id="home"
+            className="min-h-screen flex items-center justify-center relative"
+        >
+            <div className="text-center z-10 px-4">
+                <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-600 to-blue-400 bg-clip-text text-transparent  leading-right">
+                    Hi, I'm Justin
+                </h1>
+
+                <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto">
+                    I am currently working as a Digital Transformation Intern at the New Jersey Department of Community Affairs. I graduated with a Bachelor's degree in both Computer Science and Psychology degree at Rutgers-New Brunswick and have a solid foundation in software development, data structure, and web development.  I also an excellent understanding in psychoogical theories, research methods, and data collection. As a Digital Transformation Intern, I have built and launched multiple automated workflows in SimpliGov, streamlining data intake and reducing manual processing for the public, employees, and stakeholders.
+                </p>
+
+                <div className="flex justify-center space-x-4">
+                    <a 
+                        href="#projects"
+                        className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden 
+                        hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"
+                    >
+                        View Projects
+                    </a>
+
+                    <a 
+                        href="#contact"
+                        className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 
+                        hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.2)] hover:bg-blue-500/10"
+                    >
+                        Contact Me
+                    </a>
+                </div>
+            </div>
+
+        </section>
+    )
+}
