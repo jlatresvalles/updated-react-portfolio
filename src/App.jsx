7757c5e561f28ae8b48605bff9b253a04617082d
jlatresvalles <jlatresvalles@gmail.com>
2025-12-16 12:09:08 -0500

    Added new features to Navbar, will start introdcution soon

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { useState } from "react";
 import './App.css';
 import { LoadingScreen } from './components/LoadingScreen';
 import { Navbar } from './components/Navbar';
+import { MobileMenu } from "./components/MobileMenu";
 import "./index.css";
 
 function App() {
-  const [isLoaded, setIsLoaded] = useState(false)
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return <>
     {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}{" "}
@@ -13,7 +15,8 @@ function App() {
       isLoaded ? "opacity-100" : "opacity-0"} 
       bg-black text-gray-100`}
     >
-      <Navbar />
+      <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
     </div>
   </>;
 }
