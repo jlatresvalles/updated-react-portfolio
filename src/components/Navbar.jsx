dfac41d3af48c17836ee80211196c6c63548bbbd
jlatresvalles <jlatresvalles@gmail.com>
2025-12-19 11:12:27 -0500

    Fixed NavBar, added full name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,10 +9,10 @@ export const Navbar = ({ menuOpen, setMenuOpen }) => {
     return (
         <nav className="fixed top-0 w-full z-40 bg-[rgba(10,10,10,0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg">
             <div className="max-w-5xl mx-auto px-4">
-                <div className="flex justify-between items-center h-16">
-                    <a href="#home" className="font-mono text-xl font-bold text-white"> 
+                <div className="flex items-center h-16 justify-center relative">
+                    <a href="#home" className="font-mono text-xl font-bold text-white z-50"> 
                         {" "}
-                        Justin <span className="text-blue-500">Tresvalles</span>
+                        Justin Lorenzo <span className="text-blue-500">Tresvalles</span>
                     </a>
 
                     {/*Phone Menu */}
